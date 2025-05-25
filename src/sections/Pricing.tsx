0eca464f614f93851fadeae2f40e207a104ea79d
anas bd <anastrying05@gmail.com>
2025-05-25 13:33:29 +0100

    changes

--- src/sections/Pricing.tsx
+++ src/sections/Pricing.tsx
@@ -1,55 +1,43 @@
-const pricingTiers = [
-  {
-    title: "Free",
-    monthlyPrice: 0,
-    buttonText: "Get started for free",
-    popular: false,
-    inverse: false,
-    features: [
-      "Up to 5 project members",
-      "Unlimited tasks and projects",
-      "2GB storage",
-      "Integrations",
-      "Basic support",
-    ],
-  },
-  {
-    title: "Pro",
-    monthlyPrice: 9,
-    buttonText: "Sign up now",
-    popular: true,
-    inverse: true,
-    features: [
-      "Up to 50 project members",
-      "Unlimited tasks and projects",
-      "50GB storage",
-      "Integrations",
-      "Priority support",
-      "Advanced support",
-      "Export support",
-    ],
-  },
-  {
-    title: "Business",
-    monthlyPrice: 19,
-    buttonText: "Sign up now",
-    popular: false,
-    inverse: false,
-    features: [
-      "Up to 5 project members",
-      "Unlimited tasks and projects",
-      "200GB storage",
-      "Integrations",
-      "Dedicated account manager",
-      "Custom fields",
-      "Advanced analytics",
-      "Export capabilities",
-      "API access",
-      "Advanced security features",
-    ],
-  },
-];
+import {pricingPlans} from "@/assets/assets"
+import {Check} from "lucide-react"
 
 export const Pricing = () => {
-  return null;
+  return (
+    <section className="py-24 ">
+      <div className="container px-8 md:px-12 lg:px-14">
+        <div className="section-heading ">
+          <h2 className="section-title">Pricing</h2>
+          <p className="section-desc mt-5 ">
+            Lorem ipsum dolor sit amet consectetur adipisicing. Lorem ipsum dolor, sit amet consectetur adipisicing.
+          </p>
+        </div>
+        <div className="flex flex-col gap-6 items-center mt-10 lg:flex-row lg:items-end lg:justify-center">
+          {pricingPlans.map((pricing,index) => (
+            <div className={`p-10 border border-[#F1F1F1] rounded-3xl shadow-[0_7px_14px_#EAEAEA] max-w-xs w-full  ${pricing.inverse && 'bg-black text-white/70 border-black'} `} key={index}>
+              <div className="flex justify-between">
+                <h3 className={`text-lg font-bold text-black/50 ${pricing.inverse && "text-white/60"}`}>{pricing.title}</h3>
+                {pricing.popular &&
+                  <div className="inline-flex text-sm px-4 py-1.5 rounded-xl border border-white/20">
+                    <span className="bg-[linear-gradient(to_right,#DD7DD7,#31CD86,#BBCB92,#71C2EF,#3BFFFF,#DD7DDF)] font-medium bg-clip-text text-transparent ">Popular</span>
+                  </div>
+                }
+              </div>
+              <div className="flex gap-1 items-baseline mt-[30px]">
+                <span className={` text-4xl font-bold tracking-tighter leading-none ${pricing.inverse && "text-white"}`}>${pricing.monthlyPrice}</span>
+                <span className={` tracking-tight font-bold text-black/50 ${pricing.inverse && "text-white/60"}`}>/month</span>
+              </div>
+              <button className={`btn w-full mt-[30px] ${pricing.inverse && "bg-white text-black"} `}>{pricing.buttonText}</button>
+              <ul className="flex flex-col gap-5 mt-8">
+                {pricing.features.map((feature,index) => (
+                  <li key={index} className="flex text-sm gap-4 items-center">
+                    <Check className="h-6 w-6" /> <span>{feature}</span>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 };
--- src/sections/ProductShowcase.tsx
+++ src/sections/ProductShowcase.tsx
@@ -8,14 +8,14 @@ export const ProductShowcase = () => {
   return (
     <section className='overflow-x-clip bg-gradient-to-b from-white to-[#D2DCFF] py-24 '>
       <div className="container  px-8 md:px-12 lg:px-14">
-        <div className="max-w-[540px] mx-auto ">
+        <div className="section-heading">
           <div className="flex justify-center">
             <span className="tag">Boost your productivity</span>
           </div>
-          <h1 className='text-center text-3xl md:text-[54px] md:leading-[60px] lg: font-bold tracking-tighter mt-5 bg-gradient-to-b from-black to-[#001E80] bg-clip-text text-transparent '>
+          <h1 className='section-title mt-5'>
             A more effective way to track progress
           </h1>
-          <p className="text-center text-[#010D3E] mt-5 tracking-tight  text-[22px] leading-[30px] ">
+          <p className="section-desc mt-5  ">
             celebrate the joy of accomplishement   with an app designed
             to track your progress and motivate your efforts
           </p>
